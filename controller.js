bd6bd3682528c7ca1b6fc2a56e338fde9b6f413e
Paul <paul@cwik.us>
2018-12-06 01:34:55 -0600

    2nd commit. controller.js is begun

---
+++ controller.js
@@ -0,0 +1,29 @@
+// NODE DEPENDENCIES
+const axios = require('axios');
+const spotify = require('node-spotify-api');
+const moment = require('moment');
+const dotenv = require('dotenv');
+
+
+let liri = process.argv[2];
+// LIRI COMMANDS
+    // LIRI HELP
+if (liri==='commands' || 'help' || 'command' || '?' || '/h' || '/help') {
+
+}
+    // LIRI---SPOTIFY
+if (liri==='spotify') {
+
+}
+    // LIRI---OMDB
+if (liri==='omdb') {
+
+}
+    // LIRI---MOMENT
+if (liri==='moment') {
+
+}
+    // LIRI---DOTENV
+if (liri==='dotenv') {
+    
+}
